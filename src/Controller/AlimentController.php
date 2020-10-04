264f637ad53ca28f61589eb0df702f9dca4dd1b7
lamine messaci <lamine.messaci18@gmail.com>
2020-10-04 10:13:57 +0200

    Add home page for Aliments

---
+++ src/Controller/AlimentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AlimentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AlimentController extends AbstractController
+{
+    /**
+     * @Route("/", name="aliments")
+     */
+    public function index(AlimentRepository  $repository)
+    {
+        $aliments = $repository->findAll();
+        return $this->render('aliment/aliments.html.twig', [
+            'aliments' => $aliments,
+        ]);
+    }
+}
