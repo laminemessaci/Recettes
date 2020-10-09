61c280ff90d95b5e63ac616b5dc94958b1106cba
lamine messaci <lamine.messaci18@gmail.com>
2020-10-09 21:31:51 +0200

    Configure Delete Aliment

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -5,7 +5,6 @@ namespace App\Controller\Admin;
 use App\Entity\Aliment;
 use App\Form\AlimentType;
 use App\Repository\AlimentRepository;
-use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,36 +14,51 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin/aliment", name="admin_aliment")
      */
-    public function index( AlimentRepository  $repository)
+    public function index(AlimentRepository $repository)
     {
-        $aliments = $repository -> findAll();
-        return $this->render('admin/adminAliment.html.twig',[
+        $aliments = $repository->findAll();
+        return $this->render('admin/adminAliment.html.twig', [
             "aliments" => $aliments
-            ]);
+        ]);
     }
 
     /**
      * @Route("/admin/aliment/creation", name="admin_aliment_creation")
-     * @Route("/admin/aliment/{id}", name="admin_aliment_modification")
+     * @Route("/admin/aliment/{id}", name="admin_aliment_modification", methods="GET|POST")
      */
     public function ajoutModification(Aliment $aliment = null, Request $request)
     {
-        if (!$aliment){
+        if (!$aliment) {
             $aliment = new Aliment();
         }
         $manager = $this->getDoctrine()->getManager();
         $form = $this->createForm(AlimentType::class, $aliment);
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()){
-          $manager->persist($aliment);
-          $manager->flush();
-          return  $this->redirectToRoute('admin_aliment');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($aliment);
+            $manager->flush();
+            return $this->redirectToRoute('admin_aliment');
         }
 
-        return $this->render('admin/modificationAliment.html.twig',[
-            "aliment" =>$aliment,
+        return $this->render('admin/modifEtAjoutAliment.html.twig', [
+            "aliment" => $aliment,
             "form" => $form->createView(),
             "isModification" => $aliment->getId() !== null // cette condition est valable si le champ id existe en BDD
         ]);
     }
+
+    /**
+     * @Route("/admin/aliment/{id}", name="admin_aliment_suppression", methods="DELETE")
+     */
+    public function suprimerAliment(Aliment $aliment, Request $request)
+    {
+
+        if ($this->isCsrfTokenValid('SUP' . $aliment->getId(), $request->get('_token'))) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->remove($aliment);
+            $manager->flush();
+            return $this->redirectToRoute("admin_aliment");
+        }
+    }
+
 }
